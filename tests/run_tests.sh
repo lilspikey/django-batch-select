88f4c25050573e8125e44e82b04f9ab2939b9c74
John Montgomery <john@littlespikeyland.com>
2009-11-22 13:36:14 +0000

    added extra python path option so we can actually run the tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # run from parent directory (e.g. tests/run_tests.sh)
-django-admin.py test batch_select --pythonpath=. --settings=test_settings
+django-admin.py test batch_select --pythonpath=. --pythonpath=tests --settings=test_settings
