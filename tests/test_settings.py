088a295548252bca4746079a342d515bc389efe4
John Montgomery <john@littlespikeyland.com>
2009-11-24 21:17:07 +0000

    added note about django code coverage in test_settings file.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -7,6 +7,7 @@ INSTALLED_APPS = ( 'batch_select', )
 
 TESTING_BATCH_SELECT=True
 
-# enable this for coverage
-TEST_RUNNER = 'django-test-coverage.runner.run_tests'
-COVERAGE_MODULES = ('batch_select.models', 'batch_select.replay')
+# enable this for coverage (using django test coverage 
+# http://pypi.python.org/pypi/django-test-coverage )
+# TEST_RUNNER = 'django-test-coverage.runner.run_tests'
+# COVERAGE_MODULES = ('batch_select.models', 'batch_select.replay')
