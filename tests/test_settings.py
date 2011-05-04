2336ba9162e4022bae853d23dcc2ef573075612b
John Montgomery <john@littlespikeyland.com>
2011-05-04 19:38:14 +0100

    Fixed deprecation warning when running tests under 1.3

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,9 +1,20 @@
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+# Django 1.2 and less
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = ':memory:'
+# Django 1.3 and above
+DATABASES = {
+    'default': {
+        'NAME': DATABASE_NAME,
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
+
 INSTALLED_APPS = ( 'batch_select', )
+
 
 TESTING_BATCH_SELECT=True
 
